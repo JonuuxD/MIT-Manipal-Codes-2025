16c17d4c7b07d8a5844c01f08470b7cfef7d652e
Arin Gupta <112932909+JonuuxD@users.noreply.github.com>
2023-09-02 16:01:15 +0530

    Update q3.c

--- Semester 5/OS Lab/4. File System/q3.c
+++ Semester 5/OS Lab/4. File System/q3.c
@@ -2,30 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-    char existingFile[100];
-    char newLink[100];
+int main(int argc, char* argv[]) {
+     if (link(argv[1], argv[2]) == -1) {
+          perror("Error creating Hard link");
+          exit(EXIT_FAILURE);
+     }
+    printf("Hard link created successfully at:  %s\n",argv[2]);
 
-    printf("Enter the existing file name: ");
-    scanf("%s", existingFile);
-
-    //newLink must be a new file
-    printf("Enter the name for the new hard link: ");
-    scanf("%s", newLink);
-
-    if (link(existingFile, newLink) == -1) {
-        perror("Error creating hard link");
-        exit(EXIT_FAILURE);
+     if (unlink(argv[2]) == -1) {
+          perror("Error unlinking Hard link");
+          exit(EXIT_FAILURE);
     }
 
-    printf("Hard link created successfully.\n");
-
-    if (unlink(newLink) == -1) {
-        perror("Error unlinking hard link");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Hard link unlinked successfully.\n");
+    printf("soft link unlinked successfully.\n");
 
     return 0;
 }
